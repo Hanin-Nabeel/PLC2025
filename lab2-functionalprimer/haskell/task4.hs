78187b315c13858f09c465915b752065ac9e1630
Hanin Nabeel <230087860@aston.ac.uk>
2025-02-05 00:36:59 +0000

    changed task 4 to quit when typed in

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -3,8 +3,8 @@ ask prompt =
   do
   putStrLn prompt
   line <- getLine
-  if line == ""
-    then ask prompt
+  if line == "quit"
+    then putStrLn "quitting..."
     else putStrLn ("you said: " ++ reverse line)
 
 main :: IO ()
